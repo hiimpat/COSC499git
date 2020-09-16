0b6e1607372e63cf650f3d9528b3861d189365c3
hiimpat <43257489+hiimpat@users.noreply.github.com>
2020-09-16 02:40:26 -0700

    4

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 unsortarr = [1, 3, 5, 7, 9, 2, 4, 6, 8, 10]
 sortarr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+arrmin = 1
+arrmax = 10
 
 print("Unsorted array: ")
 print(unsortarr)
@@ -12,3 +14,17 @@ print(sortarr)
 
 if unsortarr == sortarr:
     print("The array is sorted")
+else:
+    print("The array is not sorted")
+
+unsortarrmin = min(unsortarr)
+unsortarrmax = max(unsortarr)
+print("The min value in the array is: ")
+print(unsortarrmin)
+print("The max value in the array is: ")
+print(unsortarrmax)
+
+if arrmin == unsortarrmin and arrmax == unsortarrmax:
+    print("These values are correct")
+else:
+    print("These values are not correct")
