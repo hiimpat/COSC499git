323fbee9d8973dd4b92ea4cd9c1cb1f688e62942
hiimpat <patrickvisubc@gmail.com>
2020-09-16 02:20:10 -0700

    3

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 
-unsortarr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+unsortarr = [1, 3, 5, 7, 9, 2, 4, 6, 8, 10]
+sortarr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+print("Unsorted array: ")
 print(unsortarr)
+
+unsortarr.sort()
+
+print("Array after sorting: ")
+print(sortarr)
+
+if unsortarr == sortarr:
+    print("The array is sorted")
